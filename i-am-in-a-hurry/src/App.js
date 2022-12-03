5a9aca3e9b11865e3876a0e85d22394694d5c9d9
박정규 <srk99279@naver.com>
2022-12-03 11:04:50 +0900

    feat : 프로젝트 생성 및 지도 생성
    카카오맵 api를 이용하여 현재 위치 표시, 현재 위치로 돌아오는 버튼 추가.

---
+++ i-am-in-a-hurry/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Location from "./components/Map";
+
+function App() {
+  return (
+    <div className="wrap-map">
+      <Location />
+    </div>
+  );
+}
+
+export default App;
